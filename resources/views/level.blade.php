613ca4bb921288d2a375464e8f5132d3e02ffcd4
Faiva Puspa Sahara <puspafai5@gmail.com>
2026-03-06 05:01:51 +0700

    praktikum 4

---
+++ resources/views/level.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data Level Pengguna</title>
+</head>
+<body>
+    <h1>Data Level Pengguna</h1>
+    <table border="1" cellpadding="5" cellspacing="0">
+        <tr>
+            <th>ID</th>
+            <th>Kode Level</th>
+            <th>Nama Level</th>
+        </tr>
+        @foreach ($levels as $level)
+        <tr>
+            <td>{{ $level->level_id }}</td>
+            <td>{{ $level->level_kode }}</td>
+            <td>{{ $level->level_nama }}</td>
+        </tr>
+        @endforeach
+    </table>
+</body>
+</html>
