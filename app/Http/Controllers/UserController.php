3c70887a1c82b21dbf5d86bb1c8b5cbbbf5e9ffd
Faiva Puspa Sahara <puspafai5@gmail.com>
2026-03-13 06:42:24 +0700

    JB4_Praktikum2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,20 +7,21 @@ use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-class UserController extends Controller
-{
-    public function index()
-    {
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager3',
-                'nama' => 'Manager Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2,
-            ],
-        );
-        $user->save();
+class UserController extends Controller{
+    public function index() {
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
 
-        return view('user', ['data' => $user]);
+        $user->username = 'manager12';
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username', 'level_id']);
+        $user->wasChanged('nama');
+        dd($user->wasChanged(['nama', 'username']));
     }
 }
